49443537268490f05185acc5c0e5c2532b113466
StefanCirovic <vladacira023@gmail.com>
2026-01-16 06:45:14 +0100

    „Vezbe

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:e_menza/providers/theme_provider.dart';
+import 'package:e_menza/widgets/subtitle_text.dart';
+import 'package:e_menza/widgets/title_text.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SubtitleTextWidget(label: "Hello"),
+            const TitelesTextWidget(label: "Hello this is me again"),
+            SwitchListTile(
+                title: Text(themeProvider.getIsDarkTheme
+                    ? "Dark Theme"
+                    : "Light Theme"),
+                value: themeProvider.getIsDarkTheme,
+                onChanged: (value) {
+                  themeProvider.setDarkTheme(themeValue: value);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/root_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+
+import 'package:e_menza/screens/home_screen.dart';
+import 'package:e_menza/screens/profile_screen.dart';
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({super.key});
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  late List<Widget> screens;
+  int currentScreen = 0;
+  late PageController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    screens = const [
+      HomeScreen(),
+      ProfileScreen(),
+    ];
+    controller = PageController(initialPage: currentScreen);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller,
+        children: screens,
+      ),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: currentScreen,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentScreen = index;
+          });
+          controller.jumpToPage(currentScreen);
+        },
+        destinations: const [
+          NavigationDestination(
+            selectedIcon: Icon(IconlyBold.home),
+            icon: Icon(IconlyLight.home),
+            label: "Home",
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(IconlyBold.profile),
+            icon: Icon(IconlyLight.profile),
+            label: "Profile",
+          )
+        ],
+      ),
+    );
+  }
+}
